1201aa517c94d7b4d1b3b3da9b314452b7ab577e
Jay Liu <jliu2589@gmail.com>
2022-10-19 23:38:14 -0400

    Removed trpc.

--- components/Compare.tsx
+++ components/Compare.tsx
@@ -3,19 +3,19 @@ import { useState } from "react";
 import { getOptions } from "../utils/getRandom";
 
 export default function Compare() {
-  const [ids, setIds] = useState(getOptions());
+  //const [ids, setIds] = useState(getOptions());
 
-  const [a, b] = ids;
+  //const [a, b] = ids;
 
   return (
     <div>
       <div className="w-6/12 mx-auto p-4 border border-gray-100 rounded drop-shadow-md">
         <div className="text-center flex flex-row justify-evenly">
           <div className="w-20 h-20 mr-2 bg-red-500 hover:bg-red-400 rounded drop-shadow-md">
-            {a}
+            A
           </div>
           <div className="w-20 h-20 mr-2 bg-blue-500 hover:bg-blue-400 rounded drop-shadow-md">
-            {b}
+            B
           </div>
         </div>
         <div className="p-3">

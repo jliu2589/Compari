d34dd4b4e078276b93783b11c6c3fedc0d099db7
Jay Liu <jliu2589@gmail.com>
2022-10-23 09:50:06 -0400

    Add vote counter

--- components/Compare.tsx
+++ components/Compare.tsx
@@ -1,28 +1,45 @@
-import React from "react";
-import { useState } from "react";
-import { getOptions } from "../utils/getRandom";
+import React from 'react';
+import { useState } from 'react';
+import { getOptions } from '../utils/getRandom';
 
 export default function Compare() {
   //const [ids, setIds] = useState(getOptions());
 
   //const [a, b] = ids;
 
+  const [voteA, setVoteA] = useState(0);
+  const [voteB, setVoteB] = useState(0);
+
+  const handleSubmitA = () => {
+    setVoteA(voteA + 1);
+  };
+
+  const handleSubmitB = () => {
+    setVoteB(voteB + 1);
+  };
+
   return (
     <div>
       <div className="w-6/12 mx-auto p-4 border border-gray-100 rounded drop-shadow-md">
         <div className="text-center flex flex-row justify-evenly">
-          <div className="w-20 h-20 mr-2 bg-red-500 hover:bg-red-400 rounded drop-shadow-md">
+          <div
+            className="w-20 h-20 mr-2 bg-red-500 hover:bg-red-400 rounded drop-shadow-md"
+            onClick={handleSubmitA}
+          >
             A
           </div>
-          <div className="w-20 h-20 mr-2 bg-blue-500 hover:bg-blue-400 rounded drop-shadow-md">
+          <div
+            className="w-20 h-20 mr-2 bg-blue-500 hover:bg-blue-400 rounded drop-shadow-md"
+            onClick={handleSubmitB}
+          >
             B
           </div>
         </div>
         <div className="p-3">
           <h2>Results </h2>
           <ul>
-            <li>A</li>
-            <li>B</li>
+            <li>A {voteA}</li>
+            <li>B {voteB}</li>
           </ul>
         </div>
       </div>
